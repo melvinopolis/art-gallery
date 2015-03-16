55bf7d7d38486ddddb7d94f867f765ba77b012ac
Mariel Van Norman <melvinopolis@gmail.com>
2015-03-16 19:15:37 -0400

    Adds user profiles and avatar links in member page and navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name,:last_name,:username,:email, :password, :password_confirmation) }
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:first_name,:last_name,:username,:email, :bio, :avatar, :password, :password_confirmation) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:first_name,:last_name,:username,:email, :bio, :avatar, :password, :password_confirmation, :current_password) }
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  	devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-         mount_uploader, :avatar AvatarUploader
+  	mount_uploader :avatar, AvatarUploader
+
+  
 end
