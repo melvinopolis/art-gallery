2839319d734895e86c598bd3bfbe2236ce37d062
Mariel Van Norman <melvinopolis@gmail.com>
2015-03-18 10:08:51 -0400

    updates to contact page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,11 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-<<<<<<< HEAD
 
   has_many :pictures
-
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-         mount_uploader :avatar, AvatarUploader
-
-  
->>>>>>> 180033d0b70c702241b8990a204a9fa8274d0563
+  mount_uploader :avatar, AvatarUploader
 end
